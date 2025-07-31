763f077cbe644250279145c5611c4062f220b2a9
GOESTERN-0974301 <u15287@glogin4.usr.hpc.gwdg.de>
2025-07-31 16:32:54 +0200

    forgotten actualization

--- modules.py
+++ modules.py
@@ -61,43 +61,6 @@ def save_cropped_boxes(image, boxes, filename, output_dir):
 
 ####old functions
 
-def yolo_to_bbox(yolo_label, img_width, img_height):
-    '''
-    converts yolo labels to values in relation to the current image
-    '''
-    class_id = yolo_label[0]
-    cx, cy, w, h = map(float, yolo_label[1:])
-    x1 = int((cx - w / 2) * img_width)
-    y1 = int((cy - h / 2) * img_height)
-    x2 = int((cx + w / 2) * img_width)
-    y2 = int((cy + h / 2) * img_height)
-    
-    return class_id, x1, y1, x2, y2
-    
-def extract_boxes(image_path, labels_path, output_path):
-    '''
-    saves bounding boxes (in yolo format) of one image as single jpgs
-    '''
-    img = cv2.imread(image_path)
-    img_height, img_width = img.shape[:2]
-
-    with open(labels_path, 'r') as f:
-        lines = f.readlines()
-
-    base_name = os.path.splitext(os.path.basename(image_path))[0]
-
-    for idx, line in enumerate(lines):
-        parts = line.strip().split()
-        class_id, x1, y1, x2, y2 = yolo_to_bbox(parts, img_width, img_height)
-        cropped = img[y1:y2, x1:x2]
-
-        # Create filename: baseName_classId_idx.jpg
-        out_name = f"{base_name}_class{class_id}_{idx}.jpg"
-        out_path = os.path.join(output_path, out_name)
-
-        cv2.imwrite(out_path, cropped)
-
-
 def get_files_by_subfolder(root_dir, count_lines=False):
     '''
     Function to get filenames (and optional line counts for labels) from subfolders
